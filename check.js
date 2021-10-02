b2024edf8c0bbf229c4faa54e52028d399ef540f
akim.egor2013@mail.ru <akim.egor2013@mail.ru>
2021-10-02 20:26:28 +0300

    feat: date

--- check.js
+++ check.js
@@ -1,12 +1,25 @@
-function createDreamTeam(members) {
-    let arr = ""
-    for (let i = 0; i < members.length; i++) {
-        if (typeof members[i] === "string") {
-            arr += members[i][0];
-        }
+function getSeason(date) {
+    if (date == undefined) return "Unable to determine the time of year!";
+    let month = date.getMonth();
+    try {
+        if (!date ||
+            !(Object.prototype.toString.call(date) === "[object Date]") ||
+            isNaN(date)
+        )
+            throw new Error("Invalid date!");
+    } catch {
+        throw new Error("Invalid date!");
     }
-    arr = arr.split("").sort().join("")
 
+    if (month == 1 - 1 || month == 2 - 1 || month == 12 - 1)
+        return "winter";
+    else if (month == 3 - 1 || month == 4 - 1 || month == 5 - 1)
+        return "spring";
+    else if (month == 9 - 1 || month == 10 - 1 || month == 11 - 1)
+        return "summer";
+    else
+        return "autumn"
 }
+const springDate = new Date(2020, 02, 31)
 
-createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null])
+alert(getSeason(springDate))
