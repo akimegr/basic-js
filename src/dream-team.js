751f8f165f1fbc62fa933e6df553b822f4389edc
akim.egor2013@mail.ru <akim.egor2013@mail.ru>
2021-10-02 20:49:04 +0300

    refacor: debug false

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,15 +13,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam( /* members */ ) {
-    function createDreamTeam(members) {
-        let arr = ""
-        for (let i = 0; i < members.length; i++) {
-            if (typeof members[i] === "string") {
-                arr += members[i][0];
-            }
-        }
-        arr = arr.split("").sort().join("")
+export default function createDreamTeam(members) {
 
+    let arr = ""
+    for (let i = 0; i < members.length; i++) {
+        if (typeof members[i] === "string") {
+            arr += members[i][0];
+        }
     }
+    arr = arr.split("").sort().join("")
+
+
 }
