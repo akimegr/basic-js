b550d6f02f98af985d427394c639097c34945b8c
akim.egor2013@mail.ru <akim.egor2013@mail.ru>
2021-10-02 20:45:15 +0300

    feat: dateNew23

--- src/what-season.js
+++ src/what-season.js
@@ -12,26 +12,26 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default function getSeason(date) {
-    function getSeason(date) {
-        if (date == undefined) return "Unable to determine the time of year!";
-        let month = date.getMonth();
-        try {
-            if (!date ||
-                !(Object.prototype.toString.call(date) === "[object Date]") ||
-                isNaN(date)
-            )
-                throw new Error("Invalid date!");
-        } catch {
-            throw new Error("Invalid date!");
-        }
 
-        if (month == 1 - 1 || month == 2 - 1 || month == 12 - 1)
-            return "winter";
-        else if (month == 3 - 1 || month == 4 - 1 || month == 5 - 1)
-            return "spring";
-        else if (month == 9 - 1 || month == 10 - 1 || month == 11 - 1)
-            return "summer";
-        else
-            return "autumn"
+    if (date == undefined) return "Unable to determine the time of year!";
+    let month = date.getMonth();
+    try {
+        if (!date ||
+            !(Object.prototype.toString.call(date) === "[object Date]") ||
+            isNaN(date)
+        )
+            throw new Error("Invalid date!");
+    } catch {
+        throw new Error("Invalid date!");
     }
+
+    if (month == 1 - 1 || month == 2 - 1 || month == 12 - 1)
+        return "winter";
+    else if (month == 3 - 1 || month == 4 - 1 || month == 5 - 1)
+        return "spring";
+    else if (month == 9 - 1 || month == 10 - 1 || month == 11 - 1)
+        return "summer";
+    else
+        return "autumn"
+
 }
