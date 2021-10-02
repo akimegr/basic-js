f44b3125bc52a201d9f6fa9c72b3545ef1698820
akim.egor2013@mail.ru <akim.egor2013@mail.ru>
2021-10-02 20:13:48 +0300

    feat: dream-team2

--- src/dream-team.js
+++ src/dream-team.js
@@ -14,14 +14,18 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function createDreamTeam( /* members */ ) {
-    function createDreamTeam(members) {
-        let arr = ""
-        for (let i = 0; i < members.length; i++) {
-            if (typeof members[i] === "string") {
-                arr += members[i][0];
-            }
-        }
-        arr = arr.split("").sort().join("")
+    let name = "";
 
-    }
+    if (!Array.isArray(members)) return false;
+
+    for (var i = 0; i < members.length; i++)
+        if (typeof members[i] == "string") {
+            var currValue = members[i].trim();
+            if (currValue.length > 0) name += currValue[0];
+        }
+
+    name = name.split("").sort().join("");
+
+    if (name == "") return false;
+    return name;
 }
