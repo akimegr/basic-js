38cc064dbf52fcbe0e504e963b4d895c9dabe451
akim.egor2013@mail.ru <akim.egor2013@mail.ru>
2021-10-02 19:39:10 +0300

    Carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample( /* sampleActivity */ ) {
+    var k = 0.693;
+
+    if (
+        isNaN(sampleActivity) ||
+        isNaN(parseFloat(sampleActivity)) ||
+        typeof sampleActivity != "string" ||
+        sampleActivity <= 0 ||
+        sampleActivity > 15
+    )
+        return false;
+
+    return Math.ceil(
+        (HALF_LIFE_PERIOD / 0.693) * Math.log(MODERN_ACTIVITY / sampleActivity)
+    );
 }
