281cee68c3ca86e9277e279094cbd5c11ce45ee1
akim.egor2013@mail.ru <akim.egor2013@mail.ru>
2021-10-02 21:45:45 +0300

    feat: chain

--- check.js
+++ check.js
@@ -1,41 +1,13 @@
-function transform(arr) {
-    var arrNew = [];
-    var Next = false;
-    var DoubleNext = false;
-    var disc = false;
+function addLink(value) {
+    let n;
+    if (arguments.len == 0) n = "( )";
+    else if (value === undefined) n = "( undefined )";
+    else if (value === null) n = "( null )";
+    else if (Number.isNaN(value)) n = "( NaN )";
+    else n = "( " + value + " )";
+    if (this.len == 0) this.ch += n;
+    else this.ch += "~~" + n;
+    this.len++;
+    return this;
 
-    if (!Array.isArray(arr))
-        throw new Error("'arr' parameter must be an instance of the Array!");
-
-    for (var i = 0; i < arr.length; i++)
-        if (arr[i] == "--discard-next") {
-            Next = true;
-        } else if (arr[i] == "--discard-prev") {
-        if (disc) {
-            disc = false;
-            continue;
-        }
-        if (arrNew.length > 0) arrNew.pop();
-    } else if (arr[i] == "--double-next") {
-        DoubleNext = true;
-    } else if (arr[i] == "--double-prev") {
-        if (disc) {
-            disc = false;
-            continue;
-        }
-        if (arrNew.length > 0) arrNew.push(arrNew[arrNew.length - 1]);
-    } else {
-        if (Next) {
-            Next = false;
-            disc = true;
-            continue;
-        }
-        if (DoubleNext) {
-            DoubleNext = false;
-            arrNew.push(arr[i], arr[i]);
-        } else arrNew.push(arr[i]);
-        disc = false;
-    }
-
-    return arrNew;
 }
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,54 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    ch: "",
+    len: 0,
+    getLength() {
+        return this.len;
+    },
+    addLink(value) {
+        let n;
+        if (arguments.len == 0) n = "( )";
+        else if (value === undefined) n = "( undefined )";
+        else if (value === null) n = "( null )";
+        else if (Number.isNaN(value)) n = "( NaN )";
+        else n = "( " + value + " )";
+        if (this.len == 0) this.ch += n;
+        else this.ch += "~~" + n;
+        this.len++;
+        return this;
+
+    },
+    removeLink(position) {
+        if (
+            Number.isInteger(position) &&
+            position >= 1 &&
+            position <= this.len
+        ) {
+            if (this.len == 1) {
+                this.ch = "";
+                this.len = 0;
+            } else {
+                let m = this.ch.split("~~")
+                this.ch = m.slice(0, position - 1).join("~~");
+                this.len--;
+            }
+            return this;
+        } else {
+            this.ch = "";
+            this.len = 0;
+            throw new Error("You can't remove incorrect link!");
+        }
+
+    },
+    reverseChain() {
+        this.ch = this.ch.split("~~").reverse().join("~~");
+        return this;
+    },
+    finishChain() {
+        let t = this.ch;
+        this.ch = "";
+        this.len = 0;
+        return t;
+    },
 };
