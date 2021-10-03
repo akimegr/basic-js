bd80919e89b58346c62898ea0f187f0f64f3acff
akim.egor2013@mail.ru <akim.egor2013@mail.ru>
2021-10-03 14:24:16 +0300

    feat ext

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,43 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function repeater(str, options) {
+    var add = "";
+    var For = "";
+    var res = "";
+    var sep = "+";
+    var addition;
+    var strN;
+
+    if (options.repeatTimes == 0) return res;
+
+    if (options.addition !== undefined && options.additionRepeatTimes != 0) {
+        if (options.addition === null) addition = "null";
+        else addition = options.addition;
+        if (
+            options.additionRepeatTimes === undefined ||
+            options.additionRepeatTimes == 1
+        )
+            add = addition;
+        else {
+            var additionSep = "|";
+            var arr = [];
+            if (options.additionSep != undefined)
+                additionSep = options.additionSep;
+            for (var i = 0; i < options.additionRepeatTimes; i++) arr.push(addition);
+            add = arr.join(additionSep;
+            }
+        }
+
+        if (options.sep !== undefined) sep = options.sep;
+        For = add + sep;
+
+        var resArr = [];
+        if (str === null) strN = "null";
+        else strN = str;
+        if (options.repeatTimes === undefined) resArr.push(strN);
+        else {
+            for (var i = 0; i < options.repeatTimes; i++) resArr.push(strN);
+        }
+        return resArr.join(For) + add;
+    }
