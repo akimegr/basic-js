069be9bea24f07c06813808d7ce6088d0215bedc
akim.egor2013@mail.ru <akim.egor2013@mail.ru>
2021-10-03 17:21:30 +0300

    feat: last2

--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -12,7 +12,11 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits( /* n */ ) {
-    if (n < 10) return n;
-    return getSumOfDigits(('' + n).split('').reduce((sum, el) => sum + +el, 0));
+export default function getSumOfDigits(n) {
+    if (n >= 0 && n <= 9) return n;
+
+    var s = 0;
+    var N = String(n);
+    for (var i = 0; i < N.length; i++) s += Number(N[i]);
+    return getSumOfDigits(s);
 }
